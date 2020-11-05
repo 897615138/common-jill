be54da4a4cd2969570972eb8f585a4ffa850a1d4
JillW <897615138@qq.com>
2020-11-05 10:14:03 +0800

    IntegerUtil 整数反转

--- src/main/java/jill/common/util/IntegerUtil.java
+++ src/main/java/jill/common/util/IntegerUtil.java
@@ -16,4 +16,29 @@ public class IntegerUtil {
     public static Character integerToChar(Integer i) {
         return (char) (i + 48);
     }
+
+    /**
+     * TODO
+     * Integer 翻转
+     *
+     * @param i Integer
+     * @return 翻转后的Integer
+     */
+    public static Integer reverse(Integer i) {
+        int rev = 0;
+        while (i != 0) {
+            int pop = i % 10;
+            i /= 10;
+            boolean b = rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7);
+            if (b) {
+                return 0;
+            }
+            b = rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8);
+            if (b) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
 }
