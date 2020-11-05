ec0b8e0da7b0f17b62b7d252686a16a1ab1246a6
JillW <897615138@qq.com>
2020-11-05 09:59:01 +0800

    IntegerUtil CharUtil Integer<>Character

---
+++ src/main/java/jill/common/util/CharUtil.java
@@ -0,0 +1,17 @@
+package jill.common.util;
+
+/**
+ * 字符型工具
+ * @author Jill W
+ * @date 2020/11/05
+ */
+public class CharUtil {
+    /**
+     * Character -> Integer
+     * @param c  Character
+     * @return Integer
+     */
+    public static Integer charToInteger(Character c){
+        return c-48;
+    }
+}
---
+++ src/main/java/jill/common/util/IntegerUtil.java
@@ -0,0 +1,17 @@
+package jill.common.util;
+
+/**
+ * Integer工具类
+ * @author Jill W
+ * @date 2020/11/05
+ */
+public class IntegerUtil {
+    /**
+     * Integer -> Character
+     * @param i Integer
+     * @return Character
+     */
+    public static Character integerToChar(Integer i){
+        return (char)(i+48);
+    }
+}
